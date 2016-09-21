10d3617041457e8f1399f068d5b65bf704bd058e
Nick Baone <narfanator@gmail.com>
2016-09-20 17:16:08 -0700

    Add project files.

---
+++ Maptionary/Node.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maptionary
+{
+    public class Node : Dictionary<string, Node>
+    {
+        public string leaf = null;
+
+        public Node this[string key] {
+            get {
+                //Safely get the value at the key, so we don't throw errors if the key doesn't exist.
+                Node result = new Node();
+                TryGetValue(key, out result);
+
+                //Turn non-existent keys into empty nodes on access, to support chain assignment of new keys
+                // aka - n[1][2] = 3
+                if(result == null) {
+                    Node n = new Node();
+                    this[key] = n;
+                    return n;
+                } else {
+                    return result;
+                }
+            }
+            set {
+                if(ContainsKey(key)) {
+                    this.Remove(key);
+                }
+                this.Add(key, value);
+            }
+        }
+
+        public static implicit operator bool(Node n)
+        {
+            if(n.leaf != null) {
+                return true;
+            }
+            foreach(Node _n in n.Values) {
+                //If any node in the structure has a leaf value, the structure is truthy.
+                if(_n) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static implicit operator Node(string s) {
+            Node n = new Node();
+            n.leaf = s;
+            return n;
+        }
+
+        public static implicit operator string(Node n) {
+            if(n.leaf != null) {
+                return n.leaf;
+            }
+            return null;
+        }
+    }
+}
---
+++ MaptionaryTests/ProgramTests.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Maptionary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maptionary.Tests {
+    [TestClass()]
+    public class NodeTests {
+        [TestMethod()]
+        public void SingleLevel() {
+            Node n = new Node();
+
+            Assert.IsFalse(n["key"], "Unassigned key isn't falsy");
+
+            n["key"] = "value";
+
+            Assert.IsTrue(n["key"], "Assigned key isn't truthy");
+            Assert.AreEqual((string)n["key"], "value", "Assigned key doesn't have correct value: " + n["key"]);
+            //Check the intended use case.
+            Assert.IsTrue(n["key"] == "value", "Assigned key doesn't have correct value: " + n["key"]);
+        }
+
+        [TestMethod()]
+        public void MultiLevel() {
+            Node n = new Node();
+
+            Assert.IsFalse(n["key"]["key"], "Unnassigned nested key isn't falsy");
+
+            n["key"]["key"] = "value";
+
+            Assert.IsTrue(n["key"]["key"], "Assigned nested key isn't truthy");
+            Assert.AreEqual((string)n["key"]["key"], "value", "Assigned nested key doesn't have correct value: " + n["key"]["key"]);
+            //Check the intended use case.
+            Assert.IsTrue(n["key"]["key"] == "value", "Assigned nested key doesn't have correct value: " + n["key"]["key"]);
+        }
+
+        [TestMethod()]
+        public void Multinode() {
+            Node n = new Node();
+            Node n1 = new Node();
+
+            n["key"] = n1;
+
+            Assert.AreSame(n["key"], n1, "Node assigned to key is the same");
+            Assert.IsFalse(n["key"], "Key assigned with empty node isn't falsy");
+            Assert.IsFalse(n["key"]["key"], "Unnassigned key through nested node isn't falsy");
+
+            n1["key"] = "value";
+
+            Assert.IsTrue(n["key"], "Key assigned with non-empty node isn't truthy");
+            Assert.AreEqual((string)n["key"]["key"], "value", "Key assigned through nested node doesn't have correct value: " + n["key"]["key"]);
+            //Check the intended use case.
+            Assert.IsTrue(n["key"]["key"] == "value", "Key assigned through nested node doesn't have correct value: " + n["key"]["key"]);
+
+            n1["recursion"] = n;
+            // Compiles / runs, which is a test in an of itself.
+            // TODO: Test for this in serialization
+            Assert.AreSame(n["key"]["recursion"], n, "Recursive structure doesn't cause an error");
+            Assert.AreEqual((string)n["key"]["recursion"]["key"]["key"], "value", "Key accessed through recursive structure doesn't have correct value: " + n["key"]["recursion"]["key"]["key"]);
+        }
+    }
+}
